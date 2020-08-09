92f76575490859f67a13a517662b06d669b5ad1a
Ismail Amir <Ismaili@TIM.ISTAFRICA.COM>
2020-08-09 20:04:25 +0300

    another one!

--- script.js
+++ script.js
@@ -3,32 +3,112 @@
                       "https://www.istafrica.com/uploaded/photos/new_homepage_20/main2.png",
                     "https://www.istafrica.com/uploaded/photos/new_homepage_20/main3.png",
                     "https://www.istafrica.com/uploaded/photos/new_homepage_20/main4.png",
-                    "https://www.istafrica.com/uploaded/photos/new_homepage_20/main5.png"]
+                    "https://www.istafrica.com/uploaded/photos/new_homepage_20/main5.png"];
 
 
+    var lineOne = ["International Baccalaureate",
+                    "Middle Years",
+                    "Elementary Campus"];
 
-    var slideIndex = 4;
+    var lineTwo = ["Diploma (IBDP)",
+                    "Program",
+                    "Primary Years Program"];
+
+    var lineThree = ["Grade 11 & 12",
+                    "Grade 6 - 10",
+                    "Age 3(EC) - Grade 5"];
+
+    var allLines = {firstLine: lineOne, secondLine: lineTwo, thirdLine: lineThree};
+                    
+
+    var slideIndex = 1;
+    var slideIndexZero = 0;
 
 
-const funcObj = {
+    const funcObj = {
 
-  animateBackground: function() {
-
-
-    //document.getElementById("top-big-back").style.backgroundImage = `url(${imageOne})`;
-
-
-    document.getElementById("top-big-back").style.backgroundImage = `url(${imagesBig[slideIndex]})`;
-
-    document.getElementById("top-big-back2").style.backgroundImage = `url(${imagesBig[0]})`;
-
+      animateBackground: function() {
   
-      
+        function animateContainer () {
 
-   
+          var containerMargin = document.getElementById("top-back-container").style.marginLeft;
+
+          if(containerMargin == "100%" || containerMargin == "" || containerMargin == "0" || containerMargin == "0px"){
+
+            document.getElementById("top-back-container").style.marginLeft = "-100%";
+          }else {
+            document.getElementById("top-back-container").style.marginLeft = "0";
+          }
+
+          setTimeout(animateContainer, 35000);
+
+         }
+
+
+        function animateMe () {
+          var randIndex = Math.floor((Math.random() * imagesBig.length) + 0);
+          
+
+          if(slideIndex%2 == 0 || slideIndex == 0){
+
+            document.getElementById("top-big-back").style.backgroundImage = `url(${imagesBig[randIndex]})`;
+            slideIndex++;
+          }else {
+            document.getElementById("top-big-back2").style.backgroundImage = `url(${imagesBig[randIndex]})`;
+            
+            /*var test100 = document.getElementById("top-back-container").style.marginLeft;
+            document.getElementById("find-out-title-text").innerHTML = randIndex;*/
+            
+            slideIndex++;
+           }
+
+          if (slideIndex == imagesBig.length){
+            slideIndex = 1;
+          }
+
+          setTimeout(animateMe, 35000);
+
+
+
+    }
+
+    animateContainer();
+    animateMe();
 
 
   },
+
+
+
+  animateFindOutDetails: function () {
+
+    function animateDetails () {
+
+      var linesArray = document.getElementsByClassName("find-out-details-text");
+
+      linesArray[0].innerHTML = lineOne[slideIndexZero];
+      linesArray[1].innerHTML = lineTwo[slideIndexZero];
+      linesArray[2].innerHTML = lineThree[slideIndexZero];
+  
+      
+  
+      if (slideIndexZero == 2) {
+        slideIndexZero=0;
+      }else{
+        slideIndexZero++;
+      }
+      
+
+      setTimeout(animateDetails,4000);
+
+    }
+
+    animateDetails();
+
+  
+
+
+  }
 
 
 }
@@ -38,6 +118,14 @@ const funcObj = {
 
 
 funcObj.animateBackground();
+funcObj.animateFindOutDetails();
+
+
+
+
+
+/*var test100 = document.getElementById("top-back-container").style.marginLeft;
+document.getElementById("find-out-title-text").innerHTML = test100;*/
 
 
 
