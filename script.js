60080ba9d79d0b04d211c9c17381a62db8309153
Ismail Amir <Ismaili@TIM.ISTAFRICA.COM>
2020-08-15 03:30:55 +0300

    added all big images batch 1

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 
-    var imagesBig = ["https://kh8lig.am.files.1drv.com/y4mvMjldyIYQ72eI-xOxl3TDVtelh9eWmWT6sexgkRZkvegBMeYsHWMXOoTHXdhdDOwfJpjVujq7xjJNa49JL4DtVXMTsly3lb3MI1SLJBakdQcvjAoNF1JAY4jEOE_tEnxvsHqal-VuwSERen-G20BYFWl-y5kMBp0Obgxay9xTwaAFi4KFRzCgBNJJgl7FHEIGwvt_FdmiK3UTHpjnkpehg?width=3000&height=1957&cropmode=none",
-                      "https://kh8lig.am.files.1drv.com/y4mvMjldyIYQ72eI-xOxl3TDVtelh9eWmWT6sexgkRZkvegBMeYsHWMXOoTHXdhdDOwfJpjVujq7xjJNa49JL4DtVXMTsly3lb3MI1SLJBakdQcvjAoNF1JAY4jEOE_tEnxvsHqal-VuwSERen-G20BYFWl-y5kMBp0Obgxay9xTwaAFi4KFRzCgBNJJgl7FHEIGwvt_FdmiK3UTHpjnkpehg?width=3000&height=1957&cropmode=none",
-                    "https://kh8lig.am.files.1drv.com/y4mvMjldyIYQ72eI-xOxl3TDVtelh9eWmWT6sexgkRZkvegBMeYsHWMXOoTHXdhdDOwfJpjVujq7xjJNa49JL4DtVXMTsly3lb3MI1SLJBakdQcvjAoNF1JAY4jEOE_tEnxvsHqal-VuwSERen-G20BYFWl-y5kMBp0Obgxay9xTwaAFi4KFRzCgBNJJgl7FHEIGwvt_FdmiK3UTHpjnkpehg?width=3000&height=1957&cropmode=none",
-                    "https://kh8lig.am.files.1drv.com/y4mvMjldyIYQ72eI-xOxl3TDVtelh9eWmWT6sexgkRZkvegBMeYsHWMXOoTHXdhdDOwfJpjVujq7xjJNa49JL4DtVXMTsly3lb3MI1SLJBakdQcvjAoNF1JAY4jEOE_tEnxvsHqal-VuwSERen-G20BYFWl-y5kMBp0Obgxay9xTwaAFi4KFRzCgBNJJgl7FHEIGwvt_FdmiK3UTHpjnkpehg?width=3000&height=1957&cropmode=none",
-                    "https://ntjuqq.am.files.1drv.com/y4mKxN5IjPsf5M0fPqBxjEJhUfw6y1Puns04x59RMJ7mTDEI6UUs5gzu59-22dgYzJ0-Zo2t69sqNmIMHasAS_rcObiWucD2Y1IJNtqF1nOPhAX5Z0MbJ7auk1TKqGFknaKEml8dP98iF8H3f2uvOXLCt8FiEmbNMaQtj6AcA8rnOGns4znzmVuaXAxhHhc0mkf_hhsQQbLxMP1_jaLI8B75Q"];
+    var imagesBig = ["https://kh8sig.am.files.1drv.com/y4mXXNZS5bshh2VPgxJfFut2456vn2vEHeaXruQXN7WkqIfaU4VS2RULNnSoXWFx7_JM8MtK3Q8J0IFE9FBDhkGeN-dk0OXxErgNR76bplcFfJ7YI2p1ew0p8B-pgzvonH27HAJ6c678jpodVfvVPXbCffl7YthuVsixHRMQwx_K-afWR3ar0biZgdW8-lnpumAbHdjyHRrWqoziiltHd9VKg",
+                      "https://xzo5la.am.files.1drv.com/y4mfqM1DdJDHGn3UU-Hk1aSvN9g6_CRhkjJXXF7SfX7-lfOICAzzhtGXYR34Qr8vhRj3QiIjwS5GKfV7pUs6DFwQJmKoGf5mJ3rSpiNL2DyincJys0aG0EK9ClwT4PHvjNAmP3oaTqi09rQhVBEMecSc7lPIb9RLYCu-xhcPR5IsTTfIy6WCykbbr2hk-pU1Qodb--vxTIGKs6lGr1LpVu-SQ",
+                    "https://asrgnw.am.files.1drv.com/y4mQ1mPg0A9pAzncM-bvq3-LUvSb8L8li3wpwJPFKEvsIFWMRDdfZJGtOZJZaf-OePepUaypuO8NhgjrDw7xKoqVLT7Weyv4Upt1RMAY-3AgZlY70gH9l1OjcxWHeLfysg-Y9id6yEqsH3n1m_x1NYJdwFX42pysKM1unq7nVmKbqpvJwEpVG-dt44AkTQxtCgQITIfR9kB66olQtt7kpPhgw",
+                    "https://ntjuqq.am.files.1drv.com/y4mKxN5IjPsf5M0fPqBxjEJhUfw6y1Puns04x59RMJ7mTDEI6UUs5gzu59-22dgYzJ0-Zo2t69sqNmIMHasAS_rcObiWucD2Y1IJNtqF1nOPhAX5Z0MbJ7auk1TKqGFknaKEml8dP98iF8H3f2uvOXLCt8FiEmbNMaQtj6AcA8rnOGns4znzmVuaXAxhHhc0mkf_hhsQQbLxMP1_jaLI8B75Q",
+                    "https://ok2v5a.am.files.1drv.com/y4m0mMCeWTC-ZjA7hn8-QVBrcqwJvAkvkkmny8q6w5j-JJ28R6409hcgXOyPnYKb2L4ZhVK4Sg7eFgjWbZeTUEUSVHERYZRZdH2WspnNr5DSLjCmHP8BwYnPHPBKex52xBhIFigGriBIxCpvxCBU57NSnApA0mbun4KxYud_gkfmroPpb0NamJRacAQVZlptfF913kuRhQ2AE4rM91aVZM2PA",
+                    "https://0ye9hw.am.files.1drv.com/y4mRsZ7i20n_z7Gm1BzOGL1uULrCf6_7JTJJYugzjW8sMLdyu6CKaUOP4oXKq3HnxCxLNbEr_YnL19aHDzMs2RZlgr6M3pvUmFZ2V85Y-tcZpKHeWC1IGxEXb_Z9soSovW5tUDNRrG5UUjPnR9sKAabv4upihC1UIR8CPViISwsqrcW54T_gPUSHnvguZWZARbPr4yqpBE-aegNrnuAFBcrwg",
+                    "https://lvgofa.am.files.1drv.com/y4m74Yh32rynjqsvh0OtZeHdwacFYdZvgcsTDciqg2XrowHIQZXZxkpuTslA7AxcNz869QGA0kkNg-q3q0IqUmwWb5Zak1FTvVXJKp7rIX5IKKHcxtDWREpHsHLqOYkQbYMWV1a3ixEN_AcfBCd0FQFkS7KVyVg58pwDh6_Qo9oOY9hgO3_7NvmtujlkR1X1E8tfZEwLTl8EuJzyePIXeFmTg",
+                    "https://mwzqtw.am.files.1drv.com/y4mSOIBIntcn7jEnnI0liY-L5L4gYe_keeu3SDWJeVWDDBhwwJOGj_Lq5YK8xBFK5XPxCGs5QRONbqczQXAqaFmL-1uxpGFkvgtQtvsyqt4kBwcYZyLufhY1v8ZIlAQRE63uwA4ya0Hx3qSGcNRz8tLJKa3dkqS70wkt41Y5HhlHxpwU0n8O1u7P9NYXzuQqlu47YzL0zyxn0cC6qxdzaVvSw",
+                    "https://znr3wq.am.files.1drv.com/y4mTjXh3_LrfW0PZDJUJKEsrYainHOs6U8Kv4HQ2X-5VcKK7LxwAkznPOZPNlkGR7hbrKOlBbqV95DMqftLs0PKKQdz59UQFmVjL-BNf3r0sDJafh1O7NU9VVjBZd-OtrYToDnl3Y7e9TMB1ourrJN2KnAjrrNPS9A_T14SF20HxcX9RKokNcSG1M5WVvskR-sXtGBCIGiF8AtgP_NEhgqxwA",
+                    "https://z0pfqa.am.files.1drv.com/y4mRmvrp8erHKeVXAg4L_Gx5DbIyPVzNtgKogtUKtewF3fg2LXUme_6iO8QeszGRh8ooMew4LBBtxgZ1OZARPfIOLDdLzV--hmORv-YIAbHHJZFxAon0jR8uOjWzl8GBfoi15FdgVFnd6IIxENHIvv9tTSRoElQz1stnlTFpkA3IlpvPejM8GXkzVJlCb20g6W45GfgETLdMnIZpvj822QTuA",
+                    "https://kh8lig.am.files.1drv.com/y4mvMjldyIYQ72eI-xOxl3TDVtelh9eWmWT6sexgkRZkvegBMeYsHWMXOoTHXdhdDOwfJpjVujq7xjJNa49JL4DtVXMTsly3lb3MI1SLJBakdQcvjAoNF1JAY4jEOE_tEnxvsHqal-VuwSERen-G20BYFWl-y5kMBp0Obgxay9xTwaAFi4KFRzCgBNJJgl7FHEIGwvt_FdmiK3UTHpjnkpehg",
+                    "https://xzoyla.am.files.1drv.com/y4m8ZQ8Yx3s0Edb4pIiuAqSvw9BD4cXbrAV8jZHc7w6SxtFaWMoCZ84BDGZwnhfkiP9GyTWvTmGt8Wxi-zWPslvvZYRqe7dt4SZHRIqg3hqgRpHAUzDr4u1oDECgpfexdsZxw40Qpg0_vcsrWUUA_QGfgqMz_vDHH7D1uNvMoHF4GPNkz5L98jbbLYNtK_-MaEisNoWDpoAcOX15Smmdg9lJw",
+                    "https://asrhnw.am.files.1drv.com/y4mB1Q5Oe0DXExo0zI4EIQx2prDdOHCNr3e9jnMJy4-_ASZEC0J0a8bxPLOnVungA4ZJyT-SJl559w07CwoanezO6-DAJwOuIyaym8BONSOs1_ICM7TOZ7-JuYoX4DBgyg_rceUDDY19SzpMCVY0PnnsCiGuKgwWNu5DDcgaAI6pDolvqHlvQ50aIf__N8Gnrj6orYg5r7_wFndCYR5smA-dg",
+                    "https://ntjvqq.am.files.1drv.com/y4mMQR-H_8DItAmxWPFKukhqwH-_QH0A9CM7XX46ymvpCz593crf_iVzW1IiyxzL0KX9XPRnjuhFoRgfnqG9y6tNpiJ0KAAPUkaiXwrDejVYy_oM6EusXk-QCYnsZ-nWmCS35ZOdLPNA-usNpcX7xW98cIKW5MZBbk7sI1X3l_UPYhN-lc3B8uBmEttC3UFYXk17v-qYL7aJmX7eQrNO3T5Cw"];
 
     var imagesCaption = { "0": ["International Day, circa 2019!","button-right-bottom"],
                           "1": ["A random student peeking curiously at the camera!","button-left-top"],
                           "2": ["Dr. Jane Goodall meets with fans during one of her visits","button-right-top"],
                           "3": ["Grade 10 Students Fair 2019","button-right-bottom"],
-                          "4": ["Tree Planting 2019","button-right-bottom"]};
+                          "4": ["Tree Planting 2019","button-right-bottom"],
+                          "5": ["Tree Planting 2019","button-right-bottom"],
+                          "6": ["Tree Planting 2019","button-right-bottom"],
+                          "7": ["Tree Planting 2019","button-right-bottom"],
+                          "8": ["Tree Planting 2019","button-right-bottom"],
+                          "9": ["Tree Planting 2019","button-right-bottom"],
+                          "10": ["Tree Planting 2019","button-right-bottom"],
+                          "11": ["Tree Planting 2019","button-right-bottom"],
+                          "12": ["Tree Planting 2019","button-right-bottom"],
+                          "13": ["Tree Planting 2019","button-right-bottom"]};
                           
 
                   /*never use left bottom here!!*/ 
