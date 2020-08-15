75d76ea0be9de4a63cc9e8bccd2309d3c7b5e4e4
Ismail Amir <Ismaili@TIM.ISTAFRICA.COM>
2020-08-15 10:38:31 +0300

    added event listeners and mobile images

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 
-    var imagesBig = ["https://kh8sig.am.files.1drv.com/y4mXXNZS5bshh2VPgxJfFut2456vn2vEHeaXruQXN7WkqIfaU4VS2RULNnSoXWFx7_JM8MtK3Q8J0IFE9FBDhkGeN-dk0OXxErgNR76bplcFfJ7YI2p1ew0p8B-pgzvonH27HAJ6c678jpodVfvVPXbCffl7YthuVsixHRMQwx_K-afWR3ar0biZgdW8-lnpumAbHdjyHRrWqoziiltHd9VKg",
-                      "https://xzo5la.am.files.1drv.com/y4mfqM1DdJDHGn3UU-Hk1aSvN9g6_CRhkjJXXF7SfX7-lfOICAzzhtGXYR34Qr8vhRj3QiIjwS5GKfV7pUs6DFwQJmKoGf5mJ3rSpiNL2DyincJys0aG0EK9ClwT4PHvjNAmP3oaTqi09rQhVBEMecSc7lPIb9RLYCu-xhcPR5IsTTfIy6WCykbbr2hk-pU1Qodb--vxTIGKs6lGr1LpVu-SQ",
-                    "https://asrgnw.am.files.1drv.com/y4mQ1mPg0A9pAzncM-bvq3-LUvSb8L8li3wpwJPFKEvsIFWMRDdfZJGtOZJZaf-OePepUaypuO8NhgjrDw7xKoqVLT7Weyv4Upt1RMAY-3AgZlY70gH9l1OjcxWHeLfysg-Y9id6yEqsH3n1m_x1NYJdwFX42pysKM1unq7nVmKbqpvJwEpVG-dt44AkTQxtCgQITIfR9kB66olQtt7kpPhgw",
-                    "https://ntjuqq.am.files.1drv.com/y4mKxN5IjPsf5M0fPqBxjEJhUfw6y1Puns04x59RMJ7mTDEI6UUs5gzu59-22dgYzJ0-Zo2t69sqNmIMHasAS_rcObiWucD2Y1IJNtqF1nOPhAX5Z0MbJ7auk1TKqGFknaKEml8dP98iF8H3f2uvOXLCt8FiEmbNMaQtj6AcA8rnOGns4znzmVuaXAxhHhc0mkf_hhsQQbLxMP1_jaLI8B75Q",
-                    "https://ok2v5a.am.files.1drv.com/y4m0mMCeWTC-ZjA7hn8-QVBrcqwJvAkvkkmny8q6w5j-JJ28R6409hcgXOyPnYKb2L4ZhVK4Sg7eFgjWbZeTUEUSVHERYZRZdH2WspnNr5DSLjCmHP8BwYnPHPBKex52xBhIFigGriBIxCpvxCBU57NSnApA0mbun4KxYud_gkfmroPpb0NamJRacAQVZlptfF913kuRhQ2AE4rM91aVZM2PA",
-                    "https://0ye9hw.am.files.1drv.com/y4mRsZ7i20n_z7Gm1BzOGL1uULrCf6_7JTJJYugzjW8sMLdyu6CKaUOP4oXKq3HnxCxLNbEr_YnL19aHDzMs2RZlgr6M3pvUmFZ2V85Y-tcZpKHeWC1IGxEXb_Z9soSovW5tUDNRrG5UUjPnR9sKAabv4upihC1UIR8CPViISwsqrcW54T_gPUSHnvguZWZARbPr4yqpBE-aegNrnuAFBcrwg",
-                    "https://kh8lig.am.files.1drv.com/y4mdWxk3culonTwR30Dg65o2OSQJ2oi4HWnGHqiVxxqMSZsFZU2hqyFbI0wT1bSIOvVYUVC94nrkPfzDSR_aUJzfMAyjqt1GaYiDGBvX7HXkPGeqqZmDgbrUUG_j0m9koEkwQbXjEOAeGbvvwvV_qFlbyoiS_lzMmDjHz0K6Z1gfVhtCrVsd0kB8pHZ-1SOtXOvOC9IO6ODptdaaU5w7DmoRw"];
+    var imagesBig = ["https://csrgpw.am.files.1drv.com/y4m0ThqgdHjddy0bV8-xdgkusaKAM-IuTyAGemQVVR8V7n3IdEFpcOA_lPPn-YsnDwddpTDoxN2d1pyBzNziqKw7G9kdBwucGKtH49SGaxQE7feyIsaFKMihPtRIeNGHh2qapMVXbJM7a5qyPy26K1MzLBL7zt42lYc4kDqMp4bxRQAXfX4l01SmOOkdgl7Ik1Nzmp4V5yhwiiy6DnXPx4XxA",
+                      "https://kh8lig.am.files.1drv.com/y4mdWxk3culonTwR30Dg65o2OSQJ2oi4HWnGHqiVxxqMSZsFZU2hqyFbI0wT1bSIOvVYUVC94nrkPfzDSR_aUJzfMAyjqt1GaYiDGBvX7HXkPGeqqZmDgbrUUG_j0m9koEkwQbXjEOAeGbvvwvV_qFlbyoiS_lzMmDjHz0K6Z1gfVhtCrVsd0kB8pHZ-1SOtXOvOC9IO6ODptdaaU5w7DmoRw",
+                    "https://xzo5la.am.files.1drv.com/y4mlYkEMwhv5Uz5Vq1NzI17b6u1PMF2nAwtP-jjVFj5Ym4mlKEhDQb0ASVqPWq8xrad3m0h1kHu6FifrT4JzbFnKEe6UmPgRGKo60ucC52QoR_PQQyWReH14YE33DJEOWojZjXC6YuTVkSxT0j4GqTmxaYrjBDJsYts2--kqx_znzbHCt-vqvin_z6S8e4vZmgNoAAFJolOVX6TANKDfwx2sA",
+                    "https://ok2v5a.am.files.1drv.com/y4mQleizFY3zWLowtJY4-JsXfGdzti3dNh4vdAf5B_UB4ioM0hG1Yo3E6uq74LhqP12jcq_EZejBbykXnzHlw-Qqs3z9HjP2OKR7YMDQGPqf6seND0QsBbtyIOhU92UjwsgqpwGk3kY3cGcWP9HLJriJJ5LGwBDJuFjoa0E1l3GV-FvMrtT_aPOCYgAoaDmyXHzDd1BIx9jONWkVEvPq0zJwQ",
+                    "https://xzoyla.am.files.1drv.com/y4mKyoqL54BUaOQtJ6ibPVH8wIvDMBGBQj3WpG37sMEugAsxGprxe7X4MX25CdEoYCz4T-M9FWijeTHguNKUdEebis0-tVsCEVego3ja82iFY5YsaS-K_RsjrlfejADs66r9NGeVXk6Bjm3UVqEcoeeeW8sBSbX4o6iwecLM7YoFyrXo1KLGZMrFhHcBN6XtnFCyPZjH3aEFhNWE8KulP10Dw",
+                    "https://0ye9hw.am.files.1drv.com/y4m-0Ta0XiF6tln-niyJpGrcxCUzr_CMko2JlpFVia81K8gHdrHTU5VcuCxqY0Ws_QcvQgpeyOh52nUsgTfiM3nUKb-8XhY_Y4EdfK9iQ0ZksbwMgvvkVAnwCyPz9EuJfM1S5DQynp2T4FEiIZjKyfbq2tOVXFidldlNhiigBlhdDUydwK89yeXkf6DRaenmRhT9TVda7nJiPUleeZdsNMBLg",
+                    "https://ntjuqq.am.files.1drv.com/y4mwq10YFvR8uNuNrQEFzE0-4-5pNOAaqaWiSLstDBPB4yeaeLKdRnmft1nFN5q5LhrOGxlN-4-eJAa9dxV8zfe3_vUFuG212wSZO5UzoL15fzD6pRfPZCb_5YcvWWms7DSKZogSxZQ0KZyO2SOIgmWcOXMtQ_I0XRx3ogdc1jCh4WpHOgQrNNH2sKcPkW2v_t86UqyK0Ir9EktcHa6KzgR-Q",
+                    "https://ptjusq.am.files.1drv.com/y4mzrWEWqP1H18y3X_xERJbWXmtyjQczqF5CJWYYKgRafPTOhBQ0vfQHd1JVilvjJpAJVhfbnf12WEe55wx3KdOJN385O77KD0dT0M2I48wJkqonDlNDZs_W_6WRVx2Mi-uTgGKXklBPigzLiMzv0--cBEqF7RSD--ov51rkNiLZwYXOkYXYiwfPo2FeHMleXm3usvw-KnNZPNom2IHbDYoWg",
+                    "https://owzqvw.am.files.1drv.com/y4m-lXjb2-sAE8FtUMK-ihvhGZkuiMuiOdZL0fGL1lNMW2DqliYMAtEQqZxd1UpH0wpQFlzrEqYDdcB8EXymocw6Ta549l9SAhKcENMIviX1gd0fbjnq_GoKey2yUQQNYP3Z-uTL01_AmiQTzHqEayBixqZF7A4tbYWL6i5jTRj83irYgbKU46ioldvHTOJP8kRnKI3Unwbxe-tYEoP9fW8-A",
+                    "https://nvgoha.am.files.1drv.com/y4mwzSF2V9h8agC-nPtbkZy9r-LCFlQ913B9vEEF6FYzAqzXIfagwjMHAFmI-QIjfz6yZTusbOYT19mCRULI-mB3tNbCakPelZ75h2vS76GjDnTdDC92GtkNw2MhQu5r8RQ-iq0BKzCR2BdO5I0cat9DYBjIAvV_2CUVvl3w7RQSADd7q5Hp7X4JGr1741rWL-Da_mE3wff2LcqBayAEDUNyA" ];
 
 
-  var imagesSmall = ["https://z0plqa.am.files.1drv.com/y4mtLsYWr_0HDmO_qdazrFR30Hdzf8zueYHhw4yTnssKxt-eFFr9Ui65fmEtVpe1myOzxN6aVTqB5qq2RECw-8FEMOOFnDdCK6Vd0PIlZGZaXgkb7BaYEh7kSm3O3pz02LpV0ML8vsRm8pMCbxdlnMcKlKS8vXjNQvpZGmozy2XB0viOdy8pVsGxRfoWUG0LQTzugSMN1GPF7rmsd8YhZCNFg",
-                    "https://kr8mig.am.files.1drv.com/y4m5ZqH5Nf7t7mBD_bc_IBTwV2J8_geQ-zDxAQmZCkZmHQMuWRmWQKLmD5M5_zgrs8YzHdcPkx1vzJ2bpiFI3gw7dUViLTpfChGFyw_o9FYz6D9pOOF9rWDo5qWUtszKSeep5sLq_YABWFxMTtCAAQnJQCs21cisztt1e9ugxKrpZNhcDYnqhVqbgXO9uyiurEy6tp8XGLR0zi6TmiHkiCrfA",
-                  "https://ptjxsq.am.files.1drv.com/y4m3eUEw_M7BGAmuTPmFtU_blSIAHfuz4QAsAQ-u7u89lGct49ZdHqviEwACeJGB4-VYQq8PQhrY7E6bmNuK-nOHMv343x42S0FaOF1bcDU94VI19qT6ik8Ti_Avw3MM3Guv2m55qAYOC6pe5GitG-VB0Ag8oUsR3KeXCWZjvJuLxweDkvEgXsfG0724xW8vrMcxE-D68U5OLIV-km3J4rjwQ",
-                  "https://kh8rig.am.files.1drv.com/y4mh1SAQQ4ZK4Av55tgauq65qeOWRUn2NaCQt7RlUJo3PksV9lnf3FrYDpX2mNweERRi33c7t-H1-IzhZ4t7MhMu-G3Tp8YC5dKmWN6s46KrV7d5a87z0PKM2zka83BVKcUj9ZK-zfC_5yOMCf_kC-Xrj13TKk6Wr8ZAFWoKKQHN-V_rRrtnvk-nIZ-zaRpbhMkEYQFs1spOWhZBGx0VWLpvg",
+  var imagesSmall = ["https://b0peba.am.files.1drv.com/y4mTu7M0GGR4ZZAgWL8I2O9LgTiH5OjiwP0VTS7QoxizSrQKHQv9ZJ0s3eIOnboFUNl4BD2nYSgXaHkeGG7zKwLoTZr6WjFElfaJ6E2mYdgbgP-XN7D6GwO_4QEnxUKbb1_PE7rbhGQbP1Nw-dyWC8uB_YGOk-TPn8cBDgS2vjaVNuf4WyKz95ncIrqS8d9cZKPPEoAuZ_ByfZvTs6kurUHQw",
+                    "https://1nr9yq.am.files.1drv.com/y4mEQFnCne7OS1Hu5Sct3sozyKV_oofdlJSc09j224LUN2y387ZUwyiYNWGYS1jJSeWJNo05lPs2kgtHT3FQvfixKTlkTBVKYg_gLLTeqp_KnktR9hobUFF3B4WokfpgKFgGK2ltSGxKeetrTRTlP0X_ZwJUMRbtoLyJJlEekxzAOG7IyK0kVQjlqWAg7HV0sH67RGC7q3Z_wFVOKj_hiopTQ",
+                  "https://kr8mig.am.files.1drv.com/y4m5ZqH5Nf7t7mBD_bc_IBTwV2J8_geQ-zDxAQmZCkZmHQMuWRmWQKLmD5M5_zgrs8YzHdcPkx1vzJ2bpiFI3gw7dUViLTpfChGFyw_o9FYz6D9pOOF9rWDo5qWUtszKSeep5sLq_YABWFxMTtCAAQnJQCs21cisztt1e9ugxKrpZNhcDYnqhVqbgXO9uyiurEy6tp8XGLR0zi6TmiHkiCrfA",
+                  "https://xzo4la.am.files.1drv.com/y4mkqF_HT0dewLM_7hi0xRhMA7c1wvgRYOUsQqYOTJ2nhAuypAeAl7n3KqwmwZ5QSHFinUpOYyWapwkDrAn7_Cny4WfhrhlZo7G4HNIMWXTlfpqdT3ZEA6QbGjRkfBRc0tBelXt2NYavL__oFckNN8RRibNr4d2xjn6yuuFbuls8V-leQOwopwBx8GCRyH8Licpqu4tUNh22sHfvdM80ol0rg",
+                  "https://mwzxtw.am.files.1drv.com/y4meaLluTjf5Mj6WbDGEauyHzbGYfwsLOUeM1qhpk3sgA-pWdLk4r86YyRdJeK2RuOQ78exxALsiLkBpQlaMlsRJtNOnhPtpgQx8SW6NN4D0QX2-HqUMaYxgHbNQqULr6vGRtNlkuQuPYkysMDjCggcPMIYOnFGEvqNKu6rgr6QFMHIGYI-6BERi-7b5AHIx3Ssv6zlxLm0qOOi5fW_wYTjpg",
                   "https://nvgrha.am.files.1drv.com/y4mq-sAoZh5WxIbGYzEmEQyzYZiFjqh7E_YGhK7GMBgHggV20oEdr31PPQlK2J4Rh22r8rIlR1Ofyfsb3SkfOJkSmhcV6UdYM2eZjDaMvtqCPii80AHItTZMw5QWgSHpimS0fud5Jb0PHz6YLkWxJp5zSxsGY3N9B-VX2AgOXgm_c-KMlKHyA_LUBW7o2ZBjioz0mM-WA-rIlpgw56-Idf_Ew",
-                  "https://2yfdjw.am.files.1drv.com/y4mT1Kosq-Np5GfKZCPRzs8NBV-3k9xoza8rw23JczLUOh4i6FrpjNvP4JECSXtaaaJeD1ydotVPHCIxgshYzoAYkDfi-kb7dk3gcA1isXSgEjz7kPx3mHIuw0zwbtS-Hj9N-CzsmVYHiPvP5tNDaT2Bup4JGpdkWOqRq2OJxuWLbe1Xzet7M-io3n0mj4S3QwNB3RSs-rX0VoFYznXpGwEvA",
-                  "https://1nr9yq.am.files.1drv.com/y4mEQFnCne7OS1Hu5Sct3sozyKV_oofdlJSc09j224LUN2y387ZUwyiYNWGYS1jJSeWJNo05lPs2kgtHT3FQvfixKTlkTBVKYg_gLLTeqp_KnktR9hobUFF3B4WokfpgKFgGK2ltSGxKeetrTRTlP0X_ZwJUMRbtoLyJJlEekxzAOG7IyK0kVQjlqWAg7HV0sH67RGC7q3Z_wFVOKj_hiopTQ"];
+                  "https://ptjxsq.am.files.1drv.com/y4m3eUEw_M7BGAmuTPmFtU_blSIAHfuz4QAsAQ-u7u89lGct49ZdHqviEwACeJGB4-VYQq8PQhrY7E6bmNuK-nOHMv343x42S0FaOF1bcDU94VI19qT6ik8Ti_Avw3MM3Guv2m55qAYOC6pe5GitG-VB0Ag8oUsR3KeXCWZjvJuLxweDkvEgXsfG0724xW8vrMcxE-D68U5OLIV-km3J4rjwQ",
+                  "https://qk2v7a.am.files.1drv.com/y4mMVr32MKN1Tk1-CEKkNi_H9xsGwYS5IEBN6HCFVXQ3noAvHtBsORiziltjjIj4tR8uY6L5j-6zWevUAiPBni64Z8O0m6VYGH_wgLkkkp18hq28upiS1fPoWhd7_WFCEIIanr3A8kI-Meq9vhRif37liRo56ozjoW0oNgQOgkFuKgzqxCWpknwNIBLvlxQIhjX0h7RoUhctr12mls2Blh9Jw",
+                  "https://z0plqa.am.files.1drv.com/y4mtLsYWr_0HDmO_qdazrFR30Hdzf8zueYHhw4yTnssKxt-eFFr9Ui65fmEtVpe1myOzxN6aVTqB5qq2RECw-8FEMOOFnDdCK6Vd0PIlZGZaXgkb7BaYEh7kSm3O3pz02LpV0ML8vsRm8pMCbxdlnMcKlKS8vXjNQvpZGmozy2XB0viOdy8pVsGxRfoWUG0LQTzugSMN1GPF7rmsd8YhZCNFg",
+                  "https://2yfdjw.am.files.1drv.com/y4mT1Kosq-Np5GfKZCPRzs8NBV-3k9xoza8rw23JczLUOh4i6FrpjNvP4JECSXtaaaJeD1ydotVPHCIxgshYzoAYkDfi-kb7dk3gcA1isXSgEjz7kPx3mHIuw0zwbtS-Hj9N-CzsmVYHiPvP5tNDaT2Bup4JGpdkWOqRq2OJxuWLbe1Xzet7M-io3n0mj4S3QwNB3RSs-rX0VoFYznXpGwEvA" ];
 
 
 
 
-    var imagesCaption = { "0": ["International Day, circa 2019!","button-right-bottom"],
-                          "1": ["A random student peeking curiously at the camera!","button-left-top"],
-                          "2": ["Dr. Jane Goodall meets with fans during one of her visits","button-right-top"],
-                          "3": ["Grade 10 Students Fair 2019","button-right-bottom"],
-                          "4": ["Tree Planting 2019","button-right-bottom"],
-                          "5": ["Tree Planting 2019","button-right-bottom"],
-                          "6": ["Tree Planting 2019","button-right-bottom"],
-                          "7": ["Tree Planting 2019","button-right-bottom"],
-                          "8": ["Tree Planting 2019","button-right-bottom"],
-                          "9": ["Tree Planting 2019","button-right-bottom"],
-                          "10": ["Tree Planting 2019","button-right-bottom"],
-                          "11": ["Tree Planting 2019","button-right-bottom"],
-                          "12": ["Tree Planting 2019","button-right-bottom"],
-                          "13": ["Tree Planting 2019","button-right-bottom"]};
+    var imagesCaption = { "0": ["Children from IST and local schools' Roots and Shoots clubs meet Dr. Jane Goodall. - February 2019.","button-right-bottom"],
+                          "1": ["A Red Riding Hood scene from Into The Woods, Middle and High School Play. - December 2019","button-right-bottom"],
+                          "2": ["International Day Secondary Campus, 2019.","button-right-top"],
+                          "3": ["Siku ya Furaha, a Friday of learning and celebrating Tanzanian culture on Elementary Campus. - September 2019","button-right-top"],
+                          "4": ["Math games during ISSEA STEM 2019 at IST. - April 2019","button-right-top"],
+                          "5": ["Live performances during the joint-campus music event, Ignite. - October 2019","button-left-top"],
+                          "6": ["Learning outdoor skills with outdoor classrooms at Elementary Campus. - All year!","button-left-top"],
+                          "7": ["Training for excellent boomerang combat skills!","button-right-bottom"],
+                          "8": ["2 modern pools, 1 each campus.","button-right-bottom"],
+                          "9": ["2 fully equipped libraries, 1 each campus.","button-right-bottom"]};
                           
 
                   /*never use left bottom here!!*/ 
@@ -86,7 +88,7 @@
 
          }
 
-
+          
         function animateMe () {
           var randIndex = Math.floor((Math.random() * imagesBig.length) + 0);
           
@@ -255,19 +257,24 @@
 }
 
 funcObj.smallifyImages();
+funcObj.animateBackground();
+funcObj.animateFindOutDetails();
+
 
 
 
 let bottom = document.getElementById("footer-container");
 let topBack = document.getElementById("top-back-container");
+let findOutBox = document.getElementById("find-out-title");
+let welcomeToDiv = document.getElementById("welcome-to-div");
 
 bottom.addEventListener("click",funcObj.theButton, false);
 
 topBack.addEventListener("mouseover",funcObj.reverseHover, false);
 topBack.addEventListener("click",funcObj.resetHover, false);
+findOutBox.addEventListener("click",funcObj.resetHover, false);
+welcomeToDiv.addEventListener("click",funcObj.resetHover, false);
 
-funcObj.animateBackground();
-funcObj.animateFindOutDetails();
 
 
 
