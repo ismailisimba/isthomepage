f7595521149c4952a2ffcdf650a9d7f1a3329b21
Ismail Amir <Ismaili@TIM.ISTAFRICA.COM>
2020-11-06 07:08:58 +0300

    new files

--- newscript.js
+++ newscript.js
@@ -1,19 +1,37 @@
 
 
-var imagesBigobj = {images:["https://am3pap006files.storage.live.com/y4mqIqhkVgyFZiFP9sThf7ljSL4bDGZPESKHehhk-fr5Mii01yJ5GbJ7m2AtWRSgovxXTUd0XU494kFZk5veyPmbmaOF-8kTwFkaES1LGwgpIilm9HRwAtahJiHKpzlopc-gcUavVuBBLKnBAMSm6mPz7ZjRBU4hcwH9t_df5xcc7IyCpy_KHPCo1uTqMyUMlLf?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4m7tfdeXhNRHTukOrXTvQnn69MeQcTKMblGUVQ4GHyLsK16iVMCwuj6evAOcoNdEItWDXWcIpfdPWh6ebp6x-51dArdD0oGyDQiM96-6ucBoFjhioPYu67emQf5pVLSPwxPBktw2MQYNEIi2FD_rtq56eoPULble3Hzjuns550G1hGDha-TaqHftxIlvOW58la?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4mZESYTWNIdcHGORkSfXcNsRQQNdvleMXOTFEVPjD5Iw_LCOw7FNEmozUEBm7gNIqu_6EbmMaAiVWjFMo0lKsSSIEuHy11kzzFHHJpvAis7AxEDZ4TPkBmx0BjmWJVyr4TTrk6ATSIPOWzb7dsUStCMCFGcPOXxpV0zubccH-3GrB48oC87Wavr_jc5gOtIJH6?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4mA9sPGsSmzORjjMBo7uNsnodUFY_OKQ2OF2SK7ld76kojkDOdSYR5OW3IpLH6CCO69o6coXqQJoMRcd_qc1Ufm6tB7G-57JZB6Y4yd6ZxnqOp8XM8tUf2aAlZ8vufDG3kiSTJfxCWd1YZ2_TZYVqLPj3g1RT3eKT7Ze72HcScLCWoN1APPU19gHOiAnxECXAf?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4m3danv76TsSAzbcoYHLV9inOul-QXQfEVK8Sn6KeEixCEOL1j3gPIvSe3ySqePk7RJYS10MVTEWqCh8s-1uX5R6EaeKBBEIcWgYUAwvD0oId-KkG2OJiBDTleimHd8LWClogdl7cLF9DD0o1xbmRVnlGUtxOhQkGuyaCy5pkllTeLf6zDYaVVw364MhBkx5Dn?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4m1W89gFrsKLMerE0xoTEK9iw2jQoWag6Or3GUaPsDqmfN8quX0hzP_7pCMYecrAivamSFJWn4OwWybAeux1CHj3OIuAwjtJUtysME0YFwVeySVkywSR2AR3PFGKqbjxKF-HwAtmTemoMeGqSFSPbmz3043YKqq1mr2T6K-EWZBEkViDZYNh2CiKmfirm8RYys?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4mxl6w4opfpD1l_CW6PlRQ0igW3CJkZylLnNn9pN1oQaW0sweTgdp4MgC9j_yfTQAafcjFaVe6mo_8nJq-lsRE-Bn-mCt3EkG7vQ3vzWoZY4VcADQQ21LQyX_S5MdsEnIxCr4fTImq_Gn6eagu44UgEYesvZk5If6PozMrtVBvg-rnShQGPCeBdqpez_9FFeTE?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4mmeruYPsmmPBMmykpf8MXm4jecqh1SwYaif0sx3Gm3iDTUiEJkLiqCg7yfQYMjl2eiSXZeg_Wr2KaMEj09V1gmwfjlgTx5hPpc7O4Mw7YeiD3eeDoiu3TVeprScQjU8LEOhgvTI7qCeBrsZKN9k_AWxzE7WUWtlesQoDT0_971EqrcMUSNXp2PbhRD4nxqdxI?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4mtHsFszxSHhECVRtVnsq-2X6eiE3vTZcaubKVV6K5qgUucfRSC1TnX9INMWJpMg9bd_4xwhTagjlRDg3br3UfNbGoRixUF8OtlAEGChzyaWsDRziNMaQHM2eUmDhgtaNGATmSGOhzqVAjCoRJhH8kC0MEiB_4ED3sQtJE7meb1J-av5U5MWKDNs8Z00jLDR5i?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4m3j6mEVS3lJ9j6CmHwymlNyBMmk1jTWkB2LrkNOK8QScE_TTQ0yIWXpsbS8n5CRUCbjd7OSyYIHhyBf7LKVGNMPuvrM_VKy23l7_1gVFLRMSpXMubb6QulHq4xab-JTa-NxTLWu0MCFUv66Ebegi320Cub0YYc-0Z01g9E9l5hcE5cxwu6pizCRgTx0R1nl22?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4m6-NG71a6FR0IfPvuFG6rHz6g0k9TndN4C7BVI0FH6Hk2p835K-G5_SXyUck8hNAVcqtgfpz3uBA4NLRbfVBskP71PZASoKSrh978VuBgJbVNw3WcWp40bRO9PCvuqzf7T3mMqqMFnvPDI3s2uJVEP_DzZZ0NIPWeBx9KhdIsmxTAe6EjZaU2_QFoYayqPuS7?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4m53QLzfMk5e2zWwV1ZkXczS-GzgdFanmuZ0tFPJI-CIafKqzgpxjZu50bdl6WaXD1idomRgVutyj12EWvkMHxw2NieomHQP1XCU-8xNFhuV37yAtGgIYGre6KNy51kYjFcU1P-xUNxTLZJbU7Ta7NPSebNdNLC6vd-wXryMg2d1b7RPAGADEI1mHB036GN2SC?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4mjmZXmfhXkQXaWoPexV43_N8alaFPfk1NKNRgT8AuerBXAh_H0rrhjNs3pEFXwBn2Dl0FlS1ts4LXfteS1v2sUD9nD02vzCoHoTtydbrVp2exEPIIWj-z-MmxlAP4PYqeYlv8IkQH6Nm7TeXGPhDDKy4zQJUhjIXaBfvF5TiLjF4tL6t9VHZpmDrtqOnd3gbQ?width=1920&height=1080&cropmode=none",
-                              "https://am3pap006files.storage.live.com/y4mt0WN9-Hyjk2bWK-hkIAftuGzz_8zvJFUVNjRzPZg5FdLmq5dygFJRDvvhtpMbKacJBAtfPWfetQiaZOGJ5RYN7WsKEXafjlaiuEzcdW4WOOWXAXEWQ1-BVadkn1HoNrdI2KV5RIQXq3E5aVA0KaGs_TnZM-Zk4zSXHX8yf3ubQxAUvIQDktvWN7J8NHEA1ba?width=1920&height=1080&cropmode=none",],
+var imagesBigobj = {images:[
+"https://am3pap006files.storage.live.com/y4mNvqaklQaQ5fV0iUnQLPDS3uWL9d618RZ-ZqYnmGMqs9akx-b5j7D8VvVL2KFYWPv6jz2gVhbTpvjoOuhjUA7y74wLXW-YKixCy9Abfnx91dwQzH_5IrA1RkY8U77IH3-ELBafrAxI_Szrpb38qXjK7RAd_cbV-XuURmfSOkqu4tnDv6gTpDRRL1NhqK-qhXz?width=2500&height=1303&cropmode=none",
+"https://am3pap006files.storage.live.com/y4mi34hdVG_40Pmj5MeKo7T2XhecZ1K58SH_lS83YChUKa-hbSDHiFO_aGHCX2-BktzY2QNf2TwiUB5vwjAo22_7xiTYBQgEFlPHOGpfXbKNAu4ZtNrsmeWmoRW9ad1kZsgrc-eBZXdcovCGTS1hZjaRvDOXt0nyge6R6wn2MoqNXdWoqOuLdmo8I3D_OD5yeqS?width=2500&height=1303&cropmode=none",
+"https://am3pap006files.storage.live.com/y4mSfM7lQFytfAdebleJiKBhyjKxkhvCUL2iTkGe9VvnwypTTjZgi2xloszGTIWH9tRO6veOwClyKqcIR0ATcT9X471srfNP6XDRFZiXm_I-uL0OQnFZ5EiWIj2Nwh2N_4qcyBs1cNTO1xi_AbsWtPUlt8hSbcyBUNnZadyTZ4c6PufXxLk_OIMuCQFtdfFxPT4?width=2500&height=1303&cropmode=none",
+"https://am3pap006files.storage.live.com/y4mF9KYCXExAYR-Z5u44pVwefiGozfbODQ-FpMmL5SfotqR16kwAiPSp3RSz4OkoEl9o5jUN3HHD4qZ7vWgz-pHZCQEmPrCtHFJxsi92-o5OHJAMIbDQm6lAOSjOj9gOQkeCPeSD0bc_bUohHHo5ARQzEqAPUCJsejBdSOTUVnH55g91yF7ln8ZWfhoIdNI5NIT?width=2500&height=1303&cropmode=none",
+"https://am3pap006files.storage.live.com/y4mKFwHbcEoA-m6XLAKbeCU9LxhluSKh2O725jHDa5UL0EzeUHwf2v_ktoCGFamqLwQoH0joKb_0SIFd7Zz1Z2F6-0h6oAHfy9a53h5IE1LfZN85ZS6qHUJv7I_nGu6WEWHqheF1Kf1KgJktdJYP1-J1IzzP-bO-hYILhlXxgKh1imovq6t9poo25_axxzrBjjf?width=2500&height=1303&cropmode=none",
+"https://am3pap006files.storage.live.com/y4mNujvewLpmRtyDsjrvdckq1Vf6Dkk5hniedWi_ijUN5ogJGl7g4n9Opa2XDFyZPb-y4VtANgZ0DGaSzg_a3bcuznoff8Q1gUsmStzPre1ler6j05N3CMIVaBJVk2QU0xmXkcfOrjp90DrOm89ZcBOW8a8B8cKdc9t2Yc4afSAzG3Do9MCvKkZ1c6oNgyhA1Br?width=2500&height=1303&cropmode=none",
+"https://am3pap006files.storage.live.com/y4m2GnsOqynYkMc_-A5c-hGEO-keXJS5nb923cAS2CU4TlmtekjQHzXyy1wN86BQb4acLr6ZXDugT8ZWeHVcw8k512MXeF7WSc1vLjjxdGFBkwK_P9rpu1zZ31gSOCKuZuyxv7hcNTJrqVEM4DAVNZknRKq3DVYoJH96UqCued8FPwbXytERI_a2KPHcbSLDZWJ?width=2500&height=1303&cropmode=none",
+"https://am3pap006files.storage.live.com/y4m-kVrVYLlg-jQToXvzxnezm7vESSQmRqPnBQEp-zRMC_o1TXyH8po5WMhiEGdtPyxQtIoQ3BaJoLEjfg2vXiyzKUcSMXn2Gj9vy93eYpK9OhDWRcc41BIwdcy5M6aHFDSPEJ7MqYFhMjXTnR_RlyianO_9eRa9SPiHw9VuzrTjncyTQPhryoa4bMPU16nvb5u?width=2500&height=1303&cropmode=none",
+"https://am3pap006files.storage.live.com/y4mYWgJ5uNQcSVZ4BtJLlALLJzGJMrnJx_GPI7DF_TNCEzOYbO86fv9DBY3P4R3eiYF9eQVbxRMs7wxlXYt-NAe01lXH_uD7S6zaurFjpaX2lIPz94v5cNdpCIsSjooAaLEWk9W_al4-wkYiivaOeDS_YdzYM7KZUPYrB2b7AtaByUKANOYqT1AqctLCjoRiJjp?width=3132&height=1632&cropmode=none",
+"https://am3pap006files.storage.live.com/y4mzNSPBYknMP_90JoqHe15JyJmWif_y5XpCpJWSxoqWWiqB3ABjc89T5UgVOouGPz7FLffwWyJf5WeRlsU_nyP59Y8mpZP7nUzF316WcDuvTUzB2QPvL6FkIHW-6S8rF29DCNp-Dseul1cP2AW9_Oh4Mf9ujBYo7o6vZ5CfpayREnf_4RKVcHq5raIdsGuc3vX?width=2500&height=1303&cropmode=none",
+"https://am3pap006files.storage.live.com/y4mYriS05uH4JATnDOphlMGKRRkTAHP5ObIhPb1h9JuYZEFmznLWqC5UvPuTimQ6tmFpeFGxfpphOxV_SYG2_F0uQqlF10MyDWGHhGX0RLvOpMI34xytmFkgzZ3q6yrEJYrg2DJudlcCvVxcAnNP09Dq3szXRL5BCK1kQOcVhlcon5_VsvEL319zkJY7UjFKc5m?width=2500&height=1303&cropmode=none",
+"https://am3pap006files.storage.live.com/y4mbcgDclU8HafBiI5uYJtJRrf1S5Sl7AbL6zYVyiOKkplbAXcbXFB66bsb6QalvPlFGwNmnJ0LtssvVT5NYD6hdMqKjO1txMJ3W5YJmLPfty2hDBFmACew1fJD1FwI3WFsyXqL62-PZYVcm1ycRm_DUcTPHjNoaFI8VU2ABS59gAF2yM_1w4_1TKIsieyKKu8u?width=2500&height=1303&cropmode=none",
+"https://am3pap006files.storage.live.com/y4mjAw18fzhTnOxhgtP0a39hCRUJkkMpRyQHKNMR2XpkZMnP58tvFQUt7rQtc_ov8p9aOoIoKuNRX--sFPiuV4jL7gt1EJaB3aqeM6eOZlRwW31xctMT-sGedar7c5I1Bhd9f5NqLyi3cJ8xHk_3MlrNQ7kgYopukp4kY1jc45_UZuF4eIvJZMFtifYQ_UmlU54?width=2500&height=1303&cropmode=none"],
+
+imagesSmall:[
+  "https://b0peba.am.files.1drv.com/y4mTu7M0GGR4ZZAgWL8I2O9LgTiH5OjiwP0VTS7QoxizSrQKHQv9ZJ0s3eIOnboFUNl4BD2nYSgXaHkeGG7zKwLoTZr6WjFElfaJ6E2mYdgbgP-XN7D6GwO_4QEnxUKbb1_PE7rbhGQbP1Nw-dyWC8uB_YGOk-TPn8cBDgS2vjaVNuf4WyKz95ncIrqS8d9cZKPPEoAuZ_ByfZvTs6kurUHQw",
+  "https://1nr9yq.am.files.1drv.com/y4mEQFnCne7OS1Hu5Sct3sozyKV_oofdlJSc09j224LUN2y387ZUwyiYNWGYS1jJSeWJNo05lPs2kgtHT3FQvfixKTlkTBVKYg_gLLTeqp_KnktR9hobUFF3B4WokfpgKFgGK2ltSGxKeetrTRTlP0X_ZwJUMRbtoLyJJlEekxzAOG7IyK0kVQjlqWAg7HV0sH67RGC7q3Z_wFVOKj_hiopTQ",
+"https://kr8mig.am.files.1drv.com/y4m5ZqH5Nf7t7mBD_bc_IBTwV2J8_geQ-zDxAQmZCkZmHQMuWRmWQKLmD5M5_zgrs8YzHdcPkx1vzJ2bpiFI3gw7dUViLTpfChGFyw_o9FYz6D9pOOF9rWDo5qWUtszKSeep5sLq_YABWFxMTtCAAQnJQCs21cisztt1e9ugxKrpZNhcDYnqhVqbgXO9uyiurEy6tp8XGLR0zi6TmiHkiCrfA",
+"https://xzo4la.am.files.1drv.com/y4mkqF_HT0dewLM_7hi0xRhMA7c1wvgRYOUsQqYOTJ2nhAuypAeAl7n3KqwmwZ5QSHFinUpOYyWapwkDrAn7_Cny4WfhrhlZo7G4HNIMWXTlfpqdT3ZEA6QbGjRkfBRc0tBelXt2NYavL__oFckNN8RRibNr4d2xjn6yuuFbuls8V-leQOwopwBx8GCRyH8Licpqu4tUNh22sHfvdM80ol0rg",
+"https://mwzxtw.am.files.1drv.com/y4meaLluTjf5Mj6WbDGEauyHzbGYfwsLOUeM1qhpk3sgA-pWdLk4r86YyRdJeK2RuOQ78exxALsiLkBpQlaMlsRJtNOnhPtpgQx8SW6NN4D0QX2-HqUMaYxgHbNQqULr6vGRtNlkuQuPYkysMDjCggcPMIYOnFGEvqNKu6rgr6QFMHIGYI-6BERi-7b5AHIx3Ssv6zlxLm0qOOi5fW_wYTjpg",
+"https://nvgrha.am.files.1drv.com/y4mq-sAoZh5WxIbGYzEmEQyzYZiFjqh7E_YGhK7GMBgHggV20oEdr31PPQlK2J4Rh22r8rIlR1Ofyfsb3SkfOJkSmhcV6UdYM2eZjDaMvtqCPii80AHItTZMw5QWgSHpimS0fud5Jb0PHz6YLkWxJp5zSxsGY3N9B-VX2AgOXgm_c-KMlKHyA_LUBW7o2ZBjioz0mM-WA-rIlpgw56-Idf_Ew",
+"https://ptjxsq.am.files.1drv.com/y4m3eUEw_M7BGAmuTPmFtU_blSIAHfuz4QAsAQ-u7u89lGct49ZdHqviEwACeJGB4-VYQq8PQhrY7E6bmNuK-nOHMv343x42S0FaOF1bcDU94VI19qT6ik8Ti_Avw3MM3Guv2m55qAYOC6pe5GitG-VB0Ag8oUsR3KeXCWZjvJuLxweDkvEgXsfG0724xW8vrMcxE-D68U5OLIV-km3J4rjwQ",
+"https://qk2v7a.am.files.1drv.com/y4mMVr32MKN1Tk1-CEKkNi_H9xsGwYS5IEBN6HCFVXQ3noAvHtBsORiziltjjIj4tR8uY6L5j-6zWevUAiPBni64Z8O0m6VYGH_wgLkkkp18hq28upiS1fPoWhd7_WFCEIIanr3A8kI-Meq9vhRif37liRo56ozjoW0oNgQOgkFuKgzqxCWpknwNIBLvlxQIhjX0h7RoUhctr12mls2Blh9Jw",
+"https://z0plqa.am.files.1drv.com/y4mtLsYWr_0HDmO_qdazrFR30Hdzf8zueYHhw4yTnssKxt-eFFr9Ui65fmEtVpe1myOzxN6aVTqB5qq2RECw-8FEMOOFnDdCK6Vd0PIlZGZaXgkb7BaYEh7kSm3O3pz02LpV0ML8vsRm8pMCbxdlnMcKlKS8vXjNQvpZGmozy2XB0viOdy8pVsGxRfoWUG0LQTzugSMN1GPF7rmsd8YhZCNFg",
+"https://2yfdjw.am.files.1drv.com/y4mT1Kosq-Np5GfKZCPRzs8NBV-3k9xoza8rw23JczLUOh4i6FrpjNvP4JECSXtaaaJeD1ydotVPHCIxgshYzoAYkDfi-kb7dk3gcA1isXSgEjz7kPx3mHIuw0zwbtS-Hj9N-CzsmVYHiPvP5tNDaT2Bup4JGpdkWOqRq2OJxuWLbe1Xzet7M-io3n0mj4S3QwNB3RSs-rX0VoFYznXpGwEvA",
+  "https://am3pap006files.storage.live.com/y4m2D73yxn4hnk80FjU4ty5ukTo1Da1xiQAeZS91yJJ5Ix3tFfHsDyMn7yR5pjkFmsYe4YOIfuxu4Btvcwwe8m19ofNIUpm7TbD_BmyyFNoecKVYyAHLXMfSzx97DRxH7ziPG5YRfWlUtAwv1LZZlMLdpnZMM14rWSA4rPc2IAxyQ6460ATDiwrZOvWfyOVbPNY?width=1300&height=1900&cropmode=none",
+  "https://am3pap006files.storage.live.com/y4mGgWInrfabRx5vpenN2Xqr1UjqAhyF6O36rl3KF-EjlyHu-KNeFF96A50xa11xz-8DIRNFQUVMMo_EJDzvrQf1bxnebOk6x6ltqVbR7GMr4-Rb3GDWJNPyuk7BkteU6AISG920EEprUcq-6jgaM0F7NSL9JwDopmq5_6tCWrc5pHc0KdcgfwW7VLO98pmikXN?width=1300&height=1900&cropmode=none",
+  "https://am3pap006files.storage.live.com/y4mFNV78rlin4Wwu88YvvDXhTDtHwbXsKGZURLDpF7SshNMvfzud8JV3UJ9-lOWtNZC9i3Mpv3gLvSnXGHnCFiCRqt-hrTTHvE6JwVyHE5l3GInB-68-TuPBBzVj0avDFi6wBJ1JVbC2AC3G0fiueL-8OK5KhhJeaF-A4C25su1GcfcxvSHDXc0XhNjYcl3iZtp?width=1300&height=1900&cropmode=none",
+  "https://am3pap006files.storage.live.com/y4mO3mr01rR6-xvDZ0VXO5CetlR8y7WLYSDft62NPoxzsCCP5GHticM0J9BW1K39HU1HStytZfTRrPa8H1YY6XrCPud_8GNKv-kQw1i22W71iuPe-hizT7GIWyduHWWJWWxkquYPNxhuviHTuR0sGGvQ7_eQgTlqStbQMjVioQD-Fl-ggrYBdQwca_38mr8e8hC?width=1300&height=1900&cropmode=none",
+  "https://am3pap006files.storage.live.com/y4mfM201mFkTehdY_Je4wXDdEN8qAgUHLb-JW4l0EbXO3PSDzRRF7wLVWixshOYNU2GXZ4KDNiXyyQIJz8MVu7ZTeWXvSTh-xFW2qv4wReFxijAvrrCgS3J9pKaz3w3jE7N-71IJMGhPs3aLV1rOsqhoGSfiEbIJ_muC973KnBx8oT_A7iEV_flXOGkwOsUybA0?width=1000&height=1400&cropmode=none",
+  ],
 
                     captions:["We have two fully equipped libraries, one on each campus.",
                                 "Tennis activities after classes (ASAs) at the elementary campus. circa 2020 - 2021",
@@ -34,7 +52,17 @@ var imagesBigobj = {images:["https://am3pap006files.storage.live.com/y4mqIqhkVgy
 let counter = 0;
 let counter2 = 0;
 
+var width = window.screen.width;
 
+
+
+function checkWidth (width) {
+
+  if (width<=1024){
+
+    imagesBigobj.images = imagesBigobj.imagesSmall;
+  }
+}
 
 
 
@@ -54,17 +82,42 @@ function animateBackground () {
          divArray[0].remove();
 
       
-         divArray[1].style.left ="-100%";
+        
+
+
+         if (width<=1024){
+
+          divArray[1].style.left ="-100%";
+          divArray[2].style.left ="0px";
+
+         }else{
+
+          divArray[1].style.left ="-90%";
          divArray[2].style.left ="0px";
+
+         }
+
 
            /* Add code starts here */
            let sp1 = document.createElement("div");
            sp1.className = "top-big-back";
            sp1.style.backgroundImage = `url(${imagesBigobj.images[counter]})`;
-         
-         
-           sp1.style.width = "100%";
+
+
+           if (width<=1024){
+
+            sp1.style.width = "100.1%";
            sp1.style.left ="100%";
+
+           }else{
+
+            sp1.style.width = "90%";
+           sp1.style.left ="90%";
+
+           }
+         
+         
+           
 
            let sp2 = document.getElementById("top-child4");
            let parentDiv = sp2.parentNode;
@@ -75,12 +128,14 @@ function animateBackground () {
       if (counter == (imagesBigobj.images.length-1)){
 
         counter=0;
+      }else{
+        counter++;
       }
 
-      counter++;
+      
          
 
-     setTimeout(animateStart,4500);
+     setTimeout(animateStart,5420.69);
     }
 
      animateStart();
@@ -142,8 +197,9 @@ function theButton (buttonid) {
     
       }
 
-
+    checkWidth(width);
     animateBackground();
+    
 
     document.getElementById("footer-container").addEventListener("click", function () {theButton(this.id)});
     
